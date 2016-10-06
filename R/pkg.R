cd3e10ecb0e494868057e945247ad0b1f8899605
Emaasit <daniel.emaasit@gmail.com>
2016-10-05 23:34:34 -0700

    uploaded starter files

---
+++ R/pkg.R
@@ -0,0 +1,8 @@
+#' @title Read data from Amazon avro into Spark DataFrames
+#' @description 'sparkavro' is an extension for sparklyr to read data from Amazon avro into Spark DataFrames. It uses Spark package spark-avro to data from Amazon avro into Spark DataFrames.
+#' @name sparkavro
+#' @aliases sparkavro-package
+#' @docType package
+#' @importFrom sparklyr hive_context invoke spark_dependency register_extension sdf_register
+#' @importFrom magrittr "%>%"
+NULL
